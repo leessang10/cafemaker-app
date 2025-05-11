11f44b435312e6a928cadea68168e33bbb1fced3
leessang10 <leessang10@naver.com>
2025-05-12 01:22:02 +0900

    feat: nativewind 설정 추가

--- app/index.tsx
+++ app/index.tsx
@@ -1,9 +1,15 @@
 import { Text, View } from 'react-native';
+import '../global.css';
 
 export default function HomePage() {
   return (
-    <View>
-      <Text>홈</Text>
+    <View className={styles.container}>
+      <Text className={styles.text}>홈</Text>
     </View>
   );
 }
+
+const styles = {
+  container: 'flex-1 items-center justify-center bg-white',
+  text: 'text-4xl font-bold',
+};
