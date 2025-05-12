3d5afe300df2c880faf4f60ea50fc855a2d9145a
leessang10 <leessang10@naver.com>
2025-05-12 23:39:27 +0900

    feat: nativewind theme 설정

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,10 +1,24 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: ['./App.{js,ts,tsx}', './app/**/*.{js,ts,tsx}', './components/**/*.{js,ts,tsx}'],
-
-  presets: [require('nativewind/preset')],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        primary: '#3B82F6', // 파란색
+        secondary: '#F59E0B', // 노란색
+        gray: {
+          100: '#F5F5F5',
+          200: '#E5E5E5',
+          300: '#D4D4D4',
+          400: '#A3A3A3',
+          500: '#737373',
+          600: '#525252',
+          700: '#404040',
+          800: '#262626',
+          900: '#171717',
+        },
+      },
+    },
   },
   plugins: [],
 };
